004b07381f0ea40bda27832b22bfc821872473a3
staycozyboy <dkc984@likelion.org>
2021-02-09 16:44:11 +0900

    DL_from_Scratch3: step14

---
+++ DL_from_Scratch3/steps/step14.py
@@ -0,0 +1,82 @@
+import numpy as np
+from step13 import as_array
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()  # 1. Get a function
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
+    
+    def cleargrad(self):
+        self.grad = None
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+    
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+if __name__ == '__main__':
+    x = Variable(np.array(3.0))
+    y = add(x, x)
+    y.backward()
+    print(x.grad)
+
+    x.cleargrad()
+    y = add(add(x, x), x)
+    y.backward()
+    print(x.grad)
