9188edfff44cca89a48b59705ee4da21fc0b7414
staycozyboy <dkc984@likelion.org>
2021-02-12 01:08:32 +0900

    DL_from_Scratch3: step19(Variable Usability)

---
+++ DL_from_Scratch3/steps/step19.py
@@ -0,0 +1,90 @@
+import weakref
+import contextlib
+import numpy as np
+from step13 import as_array
+
+class Variable:
+    def __init__(self, data, name=None):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+
+        self.data = data
+        self.name = name
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+    
+    @property   # 메서드를 인스턴스 변수(? -> 멤버아닌가?)처럼 사용.
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+        
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+        
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+    
+    def cleargrad(self):
+        self.grad = None
+
+if __name__ == '__main__':
+    x = Variable(np.array([[1, 2, 3], [4, 5, 6]]), 'x')
+
+    print(x.name)
+    print(x.shape)
+    print(x)
