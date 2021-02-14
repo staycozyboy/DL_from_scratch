9abbf6b4b3439fd2134cb66cc9ee6e25475d4135
staycozyboy <dkc984@likelion.org>
2021-02-14 14:24:57 +0900

    DL_from_Scratch3: step43(Neural network)

--- DL_from_Scratch3/dezero/functions.py
+++ DL_from_Scratch3/dezero/functions.py
@@ -173,6 +173,44 @@ def matmul(x, W):
     return MatMul()(x, W)
 
 
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+    
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+# =============================================================================
+# activation function: sigmoid / relu / softmax / log_softmax / leaky_relu
+# =============================================================================
+class Sigmoid(Function):
+    def forward(self, x):
+        # xp = cuda.get_array_module(x)
+        y = 1 / (1 + np.exp(-x))
+        # xp.tanh(x * 0.5) * 0.5 + 0.5
+        return y
+    
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
 # =============================================================================
 # loss function: mean_squared_error / softmax_cross_entropy / sigmoid_cross_entropy / binary_cross_entropy
 # =============================================================================
